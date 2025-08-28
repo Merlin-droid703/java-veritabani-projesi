71e4112d8e63d871e414ffc88f8ccff5beccde52
Merlin-droid703 <melisa_aladag@hotmail.com>
2025-08-28 16:54:20 +0300

    JA-3 İlk JDBC Bağlantısı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,32 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import config.DatabaseConfig;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Connection connection = null;
+        try {
+            connection = DatabaseConfig.getConnection();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            if (connection != null && !connection.isClosed()) {
+                System.out.println("Connected!");
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+        } catch (SQLException e) {
+            System.err.println("An error occurred while checking the connection: " + e.getMessage());
+        } finally {
+
+            if (connection != null) {
+                try {
+                    connection.close();
+                    System.out.println("Connection closed.");
+                } catch (SQLException e) {
+                    System.err.println("Failed to close the connection: " + e.getMessage());
+                }
+            }
         }
     }
 }
